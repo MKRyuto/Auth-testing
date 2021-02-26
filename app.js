1606dbf9e1d4ed11ca4a7b67f762a1d7787fda70
MKRyuto <randiprawira23@gmail.com>
2021-02-26 15:11:38 +0700

    Level 2 - Encryption

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -8,24 +10,26 @@ app.use(express.static("public"));
 
 mongoose.connect('mongodb://localhost:27017/userBD', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email:String,
     password: String
-}
+});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
-})
+});
 
 app.get("/login", function(req, res){
     res.render("login");
-})
+});
 
 app.get("/register", function(req, res){
     res.render("register");
-})
+});
 
 app.post("/register", function(req, res){
     const newUser = new User({
@@ -39,7 +43,7 @@ app.post("/register", function(req, res){
         }else{
             res.render("secrets");
         }
-     })
+     });
 })
 
 app.post("/login", function(req, res){
